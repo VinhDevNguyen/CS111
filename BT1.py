bda81bfd4a3b8c76e50f22cd0f2b0eb4e6f2aed4
VinhDevNguyen <vinh2000ld14@gmail.com>
2020-03-26 21:55:51 +0700

    Fix barebone code BT1

--- BT1.py
+++ BT1.py
@@ -1,26 +1,31 @@
 def Multiple(X): 
-    Z = 0 # clear Z
-    Y = 0 # clear Y
-    temp = 0 # Clear temp
-    # Y = X
+    Z = 0 # clear Z;
+    Y = 0 # clear Y;
+    temp = 0 # Clear temp;
+    # (Y = X)
     while X != 0: # while X not 0 do
-        Y += 1 # incr Y
-        temp += 1 # incr temp
-        X -= 1 # decr X
+        Y += 1 # incr Y;
+        temp += 1 # incr temp;
+        X -= 1 # decr X;
+    # end;
     while temp != 0: # while temp not 0 do
-        X += 1 # incr X
-        temp -= 1 # decr temp
-    # Z = X * Y
+        X += 1 # incr X;
+        temp -= 1 # decr temp;
+    # end;
+    # (Z = X * Y)
     while X != 0: # while X not 0 do
-        W = 0 # clear W
+        W = 0 # clear W;
         while Y != 0: # while Y not 0 do
-            Z += 1 # incr Z
-            W += 1 # incr W
-            Y -= 1 # decr Y
+            Z += 1 # incr Z;
+            W += 1 # incr W;
+            Y -= 1 # decr Y;
+        # end;
         while W != 0: # while W not 0 do
-            Y += 1 # incr Y
-            W -= 1 # decr W
-        X -= 1 # decr X
+            Y += 1 # incr Y;
+            W -= 1 # decr W;
+        # end;
+        X -= 1 # decr X;
+    # end;
     return Z
 
 def main():
